5fa24a3cac73ed649597ab1b2bbc910fd3b392b9
Patrick <patrick.m.seiner@gmx.at>
2024-06-24 23:19:50 +0200

    Trained 5 PPO-based agents and 2 A2C-based agents Removed TD3-based agents

---
+++ example_script_A2C.py
@@ -0,0 +1,107 @@
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from fhtw_hex.submission.ppo.facade_PPO import agent_predefined, agent_random
+from fhtw_hex.submission.ppo.facade_PPO import agent_ppo4 as ppo_agent_4
+from fhtw_hex.submission.a2c.facade_A2C import agent_a2c as a2c_agent_1
+from fhtw_hex.submission.a2c.facade_A2C import agent_a2c_2 as a2c_agent_2
+
+from fhtw_hex import hex_engine as engine
+
+# Initialize a game object
+game = engine.hexPosition()
+def play_games(agent1=None, agent2=None, num_games=100):
+    results = {"Agent1 Wins": 0, "Agent2 Wins": 0}
+    for _ in range(num_games):
+        game = engine.hexPosition()
+        game.machine_vs_machine(machine1=agent1, machine2=agent2)
+        if game.winner == 1:
+            results["Agent1 Wins"] += 1
+        else:
+            results["Agent2 Wins"] += 1
+    return results 
+
+def main():
+ 
+    # Test the provided agent against a random agent
+    game.machine_vs_machine(machine1=a2c_agent_1, machine2=agent_random)
+    game.machine_vs_machine(machine1=agent_random, machine2=a2c_agent_1)
+    game.machine_vs_machine(machine1=a2c_agent_1, machine2=agent_predefined)
+    game.machine_vs_machine(machine1=agent_predefined, machine2=a2c_agent_1)
+
+    print("-------------------------------------------")
+
+    # Play games between PPO agent and random agent
+    a2c_vs_random_results = play_games(a2c_agent_1, agent_random, num_games=50)
+    random_vs_a2c_results = play_games(agent_random, a2c_agent_1, num_games=50)
+    
+    # Play games between PPO agent and predefined agent
+    a2c_vs_predefined_results = play_games(a2c_agent_1, agent_predefined, num_games=50)
+    predefined_vs_a2c_results = play_games(agent_predefined, a2c_agent_1, num_games=50)
+    
+    # Combine results for plotting
+    combined_results = {
+        "A2C(v1)\nwins vs\nRandom": a2c_vs_random_results["Agent1 Wins"] + random_vs_a2c_results["Agent2 Wins"],
+        "Random\nwins vs\nA2C(v1)": a2c_vs_random_results["Agent2 Wins"] + random_vs_a2c_results["Agent1 Wins"],
+        "A2C(v1)\nwins vs\nBase": a2c_vs_predefined_results["Agent1 Wins"] + predefined_vs_a2c_results["Agent2 Wins"],
+        "Base\nwins vs\nA2C(v1)": a2c_vs_predefined_results["Agent2 Wins"] + predefined_vs_a2c_results["Agent1 Wins"]
+        }
+
+    # Plot the results
+    labels = combined_results.keys()
+    values = combined_results.values()
+
+    plt.bar(labels, values, color=['blue', 'green', 'red', 'purple'])
+    plt.xlabel('Agents')
+    plt.ylabel('Number of Wins')
+    plt.title('A2C Agent Performance')
+    plt.show()
+
+    print("-------------------------------------------")
+    
+    # Test the provided agent against a random agent
+    game.machine_vs_machine(machine1=a2c_agent_2, machine2=agent_random)
+    game.machine_vs_machine(machine1=agent_random, machine2=a2c_agent_2)
+    game.machine_vs_machine(machine1=a2c_agent_2, machine2=ppo_agent_4)
+    game.machine_vs_machine(machine1=ppo_agent_4, machine2=a2c_agent_2)
+
+    print("-------------------------------------------")
+
+    # Play games between PPO agent and random agent
+    a2c_vs_random_results = play_games(a2c_agent_2, agent_random, num_games=50)
+    random_vs_a2c_results = play_games(agent_random, a2c_agent_2, num_games=50)
+ 
+    # Play games between PPO agent and previous PPO
+    ppo_vs_predecessor_results = play_games(a2c_agent_2, a2c_agent_1, num_games=50)
+    predecessor_vs_ppo_results = play_games(a2c_agent_1, a2c_agent_2, num_games=50)
+ 
+    # Play games between PPO agent and predefined agent
+    a2c_vs_ppo_results = play_games(a2c_agent_2, ppo_agent_4, num_games=50)
+    ppo_vs_a2c_results = play_games(ppo_agent_4, a2c_agent_2, num_games=50)
+    
+    # Combine results for plotting
+    combined_results_v2 = {
+         "A2C(v2)\nwins vs\nRandom": a2c_vs_random_results["Agent1 Wins"] + random_vs_a2c_results["Agent2 Wins"],
+         "Random\nwins vs\nA2C(v2)": a2c_vs_random_results["Agent2 Wins"] + random_vs_a2c_results["Agent1 Wins"],
+         "A2C(v2)\nwins vs\nPPO(v4)": ppo_vs_predecessor_results["Agent1 Wins"] + predecessor_vs_ppo_results["Agent2 Wins"],
+         "PPO(v4\nwins vs\nA2C(v2)": ppo_vs_predecessor_results["Agent2 Wins"] + predecessor_vs_ppo_results["Agent1 Wins"],
+         "A2C(v2)\nwins vs\nA2C(v1)": a2c_vs_ppo_results["Agent1 Wins"] + ppo_vs_a2c_results["Agent2 Wins"],
+         "A2C(v1)\nwins vs\nA2C(v2)": a2c_vs_ppo_results["Agent2 Wins"] + ppo_vs_a2c_results["Agent1 Wins"]
+     }
+
+    # Plot the results
+    labels_v2 = combined_results_v2.keys()
+    values_v2 = combined_results_v2.values()
+
+    plt.bar(labels_v2, values_v2, color=['blue', 'green', 'red', 'purple', 'yellow', 'orange'])
+    plt.xlabel('Agents')
+    plt.ylabel('Number of Wins')
+    plt.title('A2C Agent Performance')
+    plt.show()
+
+    print("-------------------------------------------")
+    
+if __name__ == "__main__":
+    main()
---
+++ fhtw_hex/submission/a2c/facade_A2C.py
@@ -0,0 +1,412 @@
+import sys
+import os
+current_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
+import math
+import random
+import numpy as np
+
+from gym import Env, spaces
+from fhtw_hex import hex_engine as engine
+from gym.wrappers import RescaleAction
+
+from stable_baselines3 import A2C, PPO
+from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+MOVES_PER_GAME = 60.5  # max possible moves per game
+TOTAL_GAMES = 50000    # max possible games per training session
+TOTAL_TIMESTEPS = int(TOTAL_GAMES * MOVES_PER_GAME)  # Adjusted total timesteps
+
+# Theis is the default Agent given
+def agent_predefined(board, action_set):
+    return action_set[0]
+
+# Define the random agent ensuring valid moves
+def agent_random(board, action_set):
+    valid_actions = [act for act in action_set if board[act[0]][act[1]] == 0]
+    if len(valid_actions):
+        #print("Number of valid actions: {}".format(len(valid_actions)))
+        return valid_actions[np.random.randint(len(valid_actions))]
+    else:
+        return None
+
+class HexEnv(Env):
+    def __init__(self, opponent=None):
+        super(HexEnv, self).__init__()
+        self.game = engine.hexPosition()
+        self.action_space = spaces.Discrete(self.game.size * self.game.size)
+        self.observation_space = spaces.Box(low=0, high=1, shape=(self.game.size, self.game.size, 1), dtype=int)
+        
+        self.current_player = random.choice([1,-1])  # Random player starts
+        self.opponent = opponent  # Opponent agent
+        
+        if self.current_player == -1:
+            print(self.opponent)
+            first_action_white = self.opponent(
+                self.game.board, 
+                self.game.get_action_space(recode_black_as_white=False))
+            self.game.moove(first_action_white)
+
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+        
+        self.game = engine.hexPosition()
+        self.current_player = random.choice([1,-1])
+    
+        if self.current_player == -1:
+            first_action_white = self.opponent(
+                self.game.board, 
+                self.game.get_action_space(recode_black_as_white=False))
+            self.game.moove(first_action_white)
+        
+        return np.array(self.game.board).reshape(self.game.size, self.game.size, 1), {}
+
+    
+    def step(self, action):
+        coordinates_player = self.game.scalar_to_coordinates (action)
+        if self.current_player == 1:
+            action_player = coordinates_player
+            
+            possible_actions = self.game.get_action_space(recode_black_as_white=True);
+            if len(possible_actions) > 0:
+                #print("Number of possible actions: {}".format(len(possible_actions)))
+                action_opponent = self.opponent(self.game.recode_black_as_white(self.game.board), possible_actions)
+                action_opponent = self.game.recode_coordinates(action_opponent)
+            else:
+                reward = 0
+                done = True
+                return np.array(self.game.board).reshape(self.game.size, self.game.size, 1), reward, done, False, {}
+        else:
+            
+            action_player = coordinates_player
+            
+            possible_actions = self.game.get_action_space(recode_black_as_white=False);
+            action_opponent=None
+            if len(possible_actions) > 0:
+                #print("Number of possible actions: {}".format(len(possible_actions)))
+                action_opponent = self.opponent(self.game.board, possible_actions)
+            else:
+                reward = 0
+                done = True
+                return np.array(self.game.board).reshape(self.game.size, self.game.size, 1), reward, done, False, {}
+                
+        if self.game.board[action_player[0]][action_player[1]] != 0:
+            # Invalid move, penalize the agent and end the episode
+            reward = -1
+            done = True
+            return np.array(self.game.board).reshape(self.game.size, self.game.size, 1), reward, done, False, {}
+        
+        self.game.moove(action_player)
+        reward = 1 if self.game.winner == self.current_player else -1 if self.game.winner == -self.current_player else 0
+        done = self.game.winner != 0
+        
+        if not done:            
+            if self.game.board[action_opponent[0]][action_opponent[1]] != 0:
+                # Invalid move, penalize the agent and end the episode
+                reward = -1
+                done = True
+                return np.array(self.game.board).reshape(self.game.size, self.game.size, 1), reward, done, False, {}
+           
+            self.game.moove(action_opponent)
+            reward = 1 if self.game.winner == self.current_player else -1 if self.game.winner == -self.current_player else 0
+            done = self.game.winner != 0
+
+        #self.render()
+        return np.array(self.game.board).reshape(self.game.size, self.game.size, 1), reward, done, False, {}
+
+    def render(self, mode='human'):
+        self.game.print()
+
+def choose_agent(board, action_set):
+    agents = [agent_random, agent_predefined]
+    current_agent = random.choice(agents)
+    return current_agent(board, action_set)
+
+def create_or_load_a2c_agent(games=5000, other_ai=choose_agent):
+    path = "A2C_{}.zip".format(games)
+    
+    if os.path.exists(path):
+        print("Loading existing model {}...".format(games))
+        agent = A2C.load(path)
+    else:
+        print("Training a new model {}...")
+        env = HexEnv(opponent=other_ai)
+        agent = A2C('MlpPolicy', env, verbose=1, n_steps=2048)
+        agent.learn(total_timesteps=games*MOVES_PER_GAME)
+        agent.save(path)
+    return agent
+
+agent_a2c_instance = create_or_load_a2c_agent(games=100001, other_ai=choose_agent)
+
+def agent_a2c(board, action_set):
+    obs = np.array(board).reshape((1, len(board), len(board), 1))
+    valid_action_found = False
+    while not valid_action_found:
+        action, _ = agent_a2c_instance.predict(obs, deterministic=True)
+        row, col = divmod(action[0], len(board))
+
+        # Ensure the selected action is valid
+        if board[row][col] == 0:
+            valid_action_found = True
+        else:
+            # If invalid, choose a valid action from the action set
+            valid_actions = [act for act in action_set if board[act[0]][act[1]] == 0]
+            #print("Number of valid actions: {}".format(len(valid_actions)))
+            if valid_actions:
+                return valid_actions[0]  # Select the first valid action
+            else:
+                raise ValueError("No valid actions available!")
+    return (row, col)
+
+def create_or_load_ppo_agent(games=5000, other_ai=choose_agent):
+    path = "PPO_{}.zip".format(games)
+    
+    if os.path.exists(path):
+        print("Loading existing model {}...".format(games))
+        agent = PPO.load(path)
+    else:
+        print("Training a new model {}...")
+        env = HexEnv(opponent=other_ai)
+        agent = PPO('MlpPolicy', env, verbose=1, n_steps=2048)
+        agent.learn(total_timesteps=games*MOVES_PER_GAME)
+        agent.save(path)
+    return agent
+
+agent_ppo_instance = create_or_load_ppo_agent(games=100004, other_ai=choose_agent)
+
+def agent_ppo(board, action_set):
+    obs = np.array(board).reshape((1, len(board), len(board), 1))
+    valid_action_found = False
+    while not valid_action_found:
+        action, _ = agent_ppo_instance.predict(obs, deterministic=True)
+        row, col = divmod(action[0], len(board))
+
+        # Ensure the selected action is valid
+        if board[row][col] == 0:
+            valid_action_found = True
+        else:
+            # If invalid, choose a valid action from the action set
+            valid_actions = [act for act in action_set if board[act[0]][act[1]] == 0]
+            #print("Number of valid actions: {}".format(len(valid_actions)))
+            if valid_actions:
+                return valid_actions[0]  # Select the first valid action
+            else:
+                raise ValueError("No valid actions available!")
+    return (row, col)
+
+def choose_agent_2(board, action_set):
+    agents = [agent_random, agent_a2c, agent_ppo]
+    current_agent = random.choice(agents)
+    return current_agent(board, action_set)
+
+agent_a2c_instance = create_or_load_a2c_agent(games=100002, other_ai=choose_agent_2)
+
+def agent_a2c_2(board, action_set):
+    obs = np.array(board).reshape((1, len(board), len(board), 1))
+    valid_action_found = False
+    while not valid_action_found:
+        action, _ = agent_a2c_instance.predict(obs, deterministic=True)
+        row, col = divmod(action[0], len(board))
+
+        # Ensure the selected action is valid
+        if board[row][col] == 0:
+            valid_action_found = True
+        else:
+            # If invalid, choose a valid action from the action set
+            valid_actions = [act for act in action_set if board[act[0]][act[1]] == 0]
+            print("Number of valid actions: {}".format(len(valid_actions)))
+            if valid_actions:
+                return valid_actions[0]  # Select the first valid action
+            else:
+                raise ValueError("No valid actions available!")
+    return (row, col)
+
+# def choose_agent_2(board, action_set):
+#     agents = [agent_random, agent_predefined, agent_ppo]
+#     current_agent = random.choice(agents)
+#     return current_agent(board, action_set)
+
+# # Instantiate the second agent
+# agent_ppo_instance_2 = create_or_load_ppo_agent(games=100002, other_ai=choose_agent_2)
+
+# def agent_ppo2(board, action_set):
+#     obs = np.array(board).reshape((1, len(board), len(board), 1))
+#     valid_action_found = False
+#     while not valid_action_found:
+#         action, _ = agent_ppo_instance_2.predict(obs, deterministic=True)
+#         row, col = divmod(action[0], len(board))
+
+#         # Ensure the selected action is valid
+#         if board[row][col] == 0:
+#             valid_action_found = True
+#         else:
+#             # If invalid, choose a valid action from the action set
+#             valid_actions = [act for act in action_set if board[act[0]][act[1]] == 0]
+#             print("Number of valid actions: {}".format(len(valid_actions)))
+#             if valid_actions:
+#                 return valid_actions[0]  # Select the first valid action
+#             else:
+#                 raise ValueError("No valid actions available!")
+#     return (row, col)
+
+# def choose_agent_3(board, action_set):
+#     agents = [agent_random, agent_predefined, agent_ppo, agent_ppo2]
+#     current_agent = random.choice(agents)
+#     return current_agent(board, action_set)
+
+# # Instantiate the second agent
+# agent_ppo_instance_3 = create_or_load_ppo_agent(games=100003, other_ai=choose_agent_3)
+
+# def agent_ppo3(board, action_set):
+#     obs = np.array(board).reshape((1, len(board), len(board), 1))
+#     valid_action_found = False
+#     while not valid_action_found:
+#         action, _ = agent_ppo_instance_2.predict(obs, deterministic=True)
+#         row, col = divmod(action[0], len(board))
+
+#         # Ensure the selected action is valid
+#         if board[row][col] == 0:
+#             valid_action_found = True
+#         else:
+#             # If invalid, choose a valid action from the action set
+#             valid_actions = [act for act in action_set if board[act[0]][act[1]] == 0]
+#             if valid_actions:
+#                 return valid_actions[0]  # Select the first valid action
+#             else:
+#                 raise ValueError("No valid actions available!")
+#     return (row, col)
+
+
+# def create_or_load_ppo_agent_150000():
+#     if os.path.exists(MODEL_PATH_150000):
+#         print("Loading existing model PPO150000...")
+#         Agent_PPO150000 = PPO.load(MODEL_PATH_150000)
+#     else:
+#         print("Training a new model PPO150000...")
+#         env = DummyVecEnv([lambda: HexEnv()])
+#         Agent_PPO100000 = PPO.load(MODEL_PATH_100000)  # Load the existing PPO100000 model
+#         env = DummyVecEnv([lambda: HexEnv(opponent=agent_random)])  # Use agent_random as the opponent
+#         Agent_PPO150000 = PPO('MlpPolicy', env, verbose=1, n_steps=2048)
+#         Agent_PPO150000.set_parameters(Agent_PPO100000.get_parameters())  # Initialize with PPO100000 parameters
+#         Agent_PPO150000.learn(total_timesteps=TOTAL_TIMESTEPS)
+#         Agent_PPO150000.save(MODEL_PATH_150000)
+#     return Agent_PPO150000
+
+# # Instantiate the third agent
+# agent_ppo_instance3 = create_or_load_ppo_agent_150000()
+
+# def agent_ppo3(board, action_set):
+#     obs = np.array(board).reshape((1, len(board), len(board), 1))
+#     valid_action_found = False
+#     while not valid_action_found:
+#         action, _ = agent_ppo_instance3.predict(obs, deterministic=True)
+#         row, col = divmod(action[0], len(board))
+
+#         # Ensure the selected action is valid
+#         if board[row][col] == 0:
+#             valid_action_found = True
+#         else:
+#             # If invalid, choose a valid action from the action set
+#             valid_actions = [act for act in action_set if board[act[0]][act[1]] == 0]
+#             if valid_actions:
+#                 return valid_actions[0]  # Select the first valid action
+#             else:
+#                 raise ValueError("No valid actions available!")
+#     return (row, col)
+
+
+# # Define the opponent agent that randomly chooses between PPO3, PPO2, and random
+# def agent_ppo2_or_ppo3_or_random(board, action_set):
+#     r = random.random()
+#     if r < 0.33:
+#         return agent_ppo3(board, action_set)
+#     elif r < 0.66:
+#         return agent_ppo2(board, action_set)
+#     else:
+#         return agent_random(board, action_set)
+
+# def create_or_load_ppo_agent_200000():
+#     if os.path.exists(MODEL_PATH_200000):
+#         print("Loading existing model PPO200000...")
+#         Agent_PPO200000 = PPO.load(MODEL_PATH_200000)
+#     else:
+#         print("Training a new model PPO200000...")
+#         env = DummyVecEnv([lambda: HexEnv(opponent=agent_ppo2_or_ppo3_or_random)])
+#         Agent_PPO150000 = PPO.load(MODEL_PATH_150000)  # Load the existing PPO150000 model
+#         Agent_PPO200000 = PPO('MlpPolicy', env, verbose=1, n_steps=2048)
+#         Agent_PPO200000.set_parameters(Agent_PPO150000.get_parameters())  # Initialize with PPO150000 parameters
+#         Agent_PPO200000.learn(total_timesteps=TOTAL_TIMESTEPS)
+#         Agent_PPO200000.save(MODEL_PATH_200000)
+#     return Agent_PPO200000
+
+# # Instantiate the fourth agent
+# agent_ppo_instance4 = create_or_load_ppo_agent_200000()
+
+# def agent_ppo4(board, action_set):
+#     obs = np.array(board).reshape((1, len(board), len(board), 1))
+#     valid_action_found = False
+#     while not valid_action_found:
+#         action, _ = agent_ppo_instance4.predict(obs, deterministic=True)
+#         row, col = divmod(action[0], len(board))
+
+#         # Ensure the selected action is valid
+#         if board[row][col] == 0:
+#             valid_action_found = True
+#         else:
+#             # If invalid, choose a valid action from the action set
+#             valid_actions = [act for act in action_set if board[act[0]][act[1]] == 0]
+#             if valid_actions:
+#                 return valid_actions[0]  # Select the first valid action
+#             else:
+#                 raise ValueError("No valid actions available!")
+#     return (row, col)
+
+
+# def agent_ppo3_or_ppo4(board, action_set):
+#     if random.random() < 0.5:
+#         return agent_ppo3(board, action_set)
+#     else:
+#         return agent_ppo4(board, action_set)
+
+# def create_or_load_ppo_agent_250000():
+#     if os.path.exists(MODEL_PATH_250000):
+#         print("Loading existing model PPO250000...")
+#         Agent_PPO250000 = PPO.load(MODEL_PATH_250000)
+#     else:
+#         print("Training a new model PPO250000...")
+#         env = DummyVecEnv([lambda: HexEnv()])
+#         Agent_PPO200000 = PPO.load(MODEL_PATH_200000)  # Load the existing PPO200000 model
+#         env = DummyVecEnv([lambda: HexEnv(opponent=agent_ppo3_or_ppo4)])  # Use the new mixed opponent function
+#         Agent_PPO250000 = PPO('MlpPolicy', env, verbose=1, n_steps=2048)
+#         Agent_PPO250000.set_parameters(Agent_PPO200000.get_parameters())  # Initialize with PPO200000 parameters
+#         Agent_PPO250000.learn(total_timesteps=TOTAL_TIMESTEPS)
+#         Agent_PPO250000.save(MODEL_PATH_250000)
+#     return Agent_PPO250000
+
+# # Instantiate the fifth agent
+# agent_ppo_instance5 = create_or_load_ppo_agent_250000()
+
+# def agent_ppo5(board, action_set):
+#     obs = np.array(board).reshape((1, len(board), len(board), 1))
+#     valid_action_found = False
+#     while not valid_action_found:
+#         action, _ = agent_ppo_instance5.predict(obs, deterministic=True)
+#         row, col = divmod(action[0], len(board))
+
+#         # Ensure the selected action is valid
+#         if board[row][col] == 0:
+#             valid_action_found = True
+#         else:
+#             # If invalid, choose a valid action from the action set
+#             valid_actions = [act for act in action_set if board[act[0]][act[1]] == 0]
+#             if valid_actions:
+#                 return valid_actions[0]  # Select the first valid action
+#             else:
+#                 raise ValueError("No valid actions available!")
+#     return (row, col)
+
+
